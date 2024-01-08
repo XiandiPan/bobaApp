86ec167395ea345c80f18076cf6c5f332e95dd89
Xiandi Pan <cindypan1807@gmail.com>
2024-01-08 18:45:22 -0500

    app.js change header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="BoBa Shop">
+        <h1>Love Boba</h1>
+        <img src="https://www.aheadofthyme.com/wp-content/uploads/2022/06/mango-bubble-tea.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My Boba shop Apps
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
