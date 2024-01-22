fbf99433838d79b1e640d5bc62d814ddae0fe2cf
Xiandi Pan <cindypan1807@gmail.com>
2024-01-22 15:38:03 -0500

    add responsive background image to the homepage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Homepage />
+        {/* <Homepage /> */}
         <RoutesList />
       </div>
     </Router>
--- src/Homepage.js
+++ src/Homepage.js
@@ -12,9 +12,16 @@ import { Link } from "react-router-dom";
 function Homepage() {
   console.debug("Homepage");
 
+    const homepageStyle = {
+      backgroundImage: 'url("https://img.freepik.com/free-vector/ad-template-bubbles-tea_52683-20018.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705795200&semt=ais")',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      minHeight: '100vh',
+    };
   return (
-    <div className="Homepage">
-      <div className="container text-center">
+    <div className="Homepage" style={homepageStyle}  >
+      <div className="container text-center" >
         <h1 className="mb-4 fw-bold">Boba shop</h1>
         <p className="lead">Your favorite Boba spot</p>
         <Link ClassName="orderButton" to="/items">order here</Link>
