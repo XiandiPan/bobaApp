a7bcef2a5fa4e97af347aff0c7b8f36483fd170c
Xiandi Pan <cindypan1807@gmail.com>
2024-01-26 17:44:54 -0500

    adding pink background style to itemDetial

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
+function ItemDetail() {
 
-function ItemDetail(){
+  const componentStyle = {
+    backgroundColor: 'pink',
+    height:'100vh'
+  };
+
   return (
     <div>
-     <p><h1>Boba ItemDetail</h1>
-      <ul>ice or hot </ul>
-      <ul>topping</ul>
-      <ul>sugar level</ul>
-     </p>
-
+      <div style={componentStyle}>
+        <Container>
+          <Row>
+            <Col xs>First, but unordered</Col>
+            <Col xs={{ order: 12 }}>Second, but last</Col>
+            <Col xs={{ order: 1 }}>Third, but second</Col>
+          </Row>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ItemDetail;
+
